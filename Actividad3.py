1a298c06ba680c8252e45d23b81843a29ef539bb
Emiliano Montalvo <xemiliano2608@gmail.com>
2025-01-24 20:15:30 -0600

    Se realizo la actividad 3, los 9 ejercicios

---
+++ Actividad3.py
@@ -0,0 +1,64 @@
+#Ejercicio 1
+print("|----------------------Ejercicio 1----------------------|")
+print("¿Cual es la funcion de la memoria secundaria en una computadora?")
+print("c) Almacenar informacion durante mucho tiempo, incluso despues de ciclos de apagado y encendido")
+
+#Ejercicio 2
+print("----------------------Ejercicio 2----------------------")
+print("¿Que es un programa?")
+print("R= Es una secuencia de declaraciones que se han echo para hacer una tarea en espesifico.")
+
+#Ejercicio 3
+print("----------------------Ejercicio 3----------------------")
+print("¿Cual es la diferencia entre un compilador y un interprete?")
+print("R= Un compilador traduce todo el código fuente a lenguaje máquina antes de ejecutar el programa, generando un archivo independiente y optimizado. En cambio, un intérprete traduce y ejecuta el código línea por línea en tiempo real, sin crear un archivo independiente, como en Python o JavaScript. ")
+
+#Ejercicio 4
+print("----------------------Ejercicio 4----------------------")
+print("¿Cual de los siguientes contiene “codigo maquina”?")
+print("a) El interprete de Python.")
+
+#Ejercicio 5
+print("----------------------Ejercicio 5----------------------")
+print("¿Que esta mal en el siguiente codigo?: primt ’¡Hola, mundo!’")
+print("La función en el código es incorrecta porque se escribió como primt en lugar de print. Además, faltan los paréntesis necesarios para llamar a la función correctamente, ya que en Python las cadenas deben ir entre comillas dentro de los paréntesis de la función print. Lo correcto seria:")
+
+print("¡Hola, mundo!")
+
+#Ejercicio 6
+print("----------------------Ejercicio 6----------------------")
+print("¿En que lugar del computador queda almacenada una variable, como en este caso “x”, despues de ejecutar la siguiente lınea de Python?:")
+x=123
+print("b) Memoria Principal.")
+
+#Ejercicio 7
+print("----------------------Ejercicio 7----------------------")
+print("¿Que mostrara en pantalla el siguiente programa?")
+x = 43
+x = x + 1
+print(x)
+
+print("b) 44")
+
+#Ejercicio 8
+print("----------------------Ejercicio 8----------------------")
+print("Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:")
+print("1. Unidad central de procesamiento.")
+print("R= Al igual que la CPU, el cerebro es el centro de procesamiento de información del cuerpo humano.")
+
+print("2. Memoria principal.")
+print("R= Es la capacidad que tenemos para retener información temporalmente mientras la usamos, como recordar un número de teléfono mientras lo marcas o lo que alguien acaba de decir.")
+
+print("3. Memoria secundaria.")
+print("R= Es donde almacenamos recuerdos y experiencias importantes de nuestra vida.")
+
+print("4. Dispositivos de entrada.")
+print("R= Al igual que los dispositivos de entrada, nuestros sentidos recogen información del entorno. Por ejemplo, nuestros ojos captan imágenes, y nuestros oídos recogen sonidos.")
+
+print("5. Dispositivos de salida.")
+print("R= Así como una computadora usa un monitor para mostrar información, nosotros usamos palabras o gestos para expresar lo que pensamos o sentimos.")
+
+#Ejercicio 9
+print("----------------------Ejercicio 9----------------------")
+print("¿Como puedes corregir un “Error de sintaxis”?")
+print("R= Para corregir un 'Error de sintaxis' en Python, leer el mensaje de error para identificar la línea del problema. Asegurarse también de que la indentación sea correcta, ya que Python depende de ella. Usar un editor de código con resaltado de sintaxis para facilitar la detección de errores. Corregir el problema y volver a ejecutar el código.")
